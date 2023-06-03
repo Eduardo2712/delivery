c459af46a524888553bb600661425831bda2be5c
Eduardo Manoel de Lara <dunabozny@gmail.com>
2023-06-03 14:03:25 -0300

    Fix Rendering

--- site/src/app/layout.tsx
+++ site/src/app/layout.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { CacheProvider } from "@chakra-ui/next-js";
-import { ChakraProvider, CSSReset, Spinner } from "@chakra-ui/react";
+import { ChakraProvider, CSSReset } from "@chakra-ui/react";
 import { themeDefault } from "../styles/theme";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
@@ -10,6 +9,7 @@ import { Metadata } from "next";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 import ProtectedRoute from "@/router";
+import { useEffect, useState } from "react";
 
 config.autoAddCss = false;
 
@@ -19,20 +19,28 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+    }, []);
+
     return (
         <html lang="pt-BR">
             <head></head>
             <body>
-                <CacheProvider>
-                    <CSSReset />
-                    <ChakraProvider theme={themeDefault}>
-                        <Provider store={store}>
-                            <ProtectedRoute>
-                                <div className="body_container">{children}</div>
-                            </ProtectedRoute>
-                        </Provider>
-                    </ChakraProvider>
-                </CacheProvider>
+                {loading && (
+                    <CacheProvider>
+                        <CSSReset />
+                        <ChakraProvider theme={themeDefault}>
+                            <Provider store={store}>
+                                <ProtectedRoute>
+                                    <div className="body_container">{children}</div>
+                                </ProtectedRoute>
+                            </Provider>
+                        </ChakraProvider>
+                    </CacheProvider>
+                )}
             </body>
         </html>
     );

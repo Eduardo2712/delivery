4298a2c21f95e3c548ad36e69a1256e79f525ae6
Eduardo Manoel de Lara <dunabozny@gmail.com>
2023-07-10 20:58:17 -0300

    Update first page

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -4,23 +4,45 @@ import { NextPage } from "next";
 
 const Page: NextPage = () => {
     return (
-        <div className="flex min-h-full flex-row justify-center mx-auto container gap-5 mt-4">
-            <div className="bg-gray-800 columns-3 flex-1 px-8 rounded-md h-24 flex justify-center flex-col items-center">
-                <p className="text-white font-extrabold text-xl">Admins</p>
+        <div className="flex min-h-full flex-col justify-center mx-auto container gap-5 mt-4 xl:flex-row px-2">
+            <div className="w-full flex flex-col md:flex-row gap-5">
+                <div className="bg-gray-800 w-full px-8 rounded-md h-24 flex justify-center flex-col items-center border-b-2 border-b-blue-600">
+                    <p className="text-gray-400 font-extrabold text-xl">Admins</p>
 
-                <p className="text-gray-400 font-bold text-xl">1</p>
+                    <p className="text-white font-bold text-xl">1</p>
+                </div>
+
+                <div className="bg-gray-800 w-full px-8 rounded-md h-24 flex justify-center flex-col items-center border-b-2 border-b-blue-600">
+                    <p className="text-gray-400 font-extrabold text-xl">Users</p>
+
+                    <p className="text-white font-bold text-xl">1</p>
+                </div>
+
+                <div className="bg-gray-800 w-full px-8 rounded-md h-24 flex justify-center flex-col items-center border-b-2 border-b-blue-600">
+                    <p className="text-gray-400 font-extrabold text-xl">Products</p>
+
+                    <p className="text-white font-bold text-xl">1</p>
+                </div>
             </div>
 
-            <div className="bg-gray-800 columns-3 flex-1 px-8 rounded-md h-24 flex justify-center flex-col items-center">
-                <p className="text-white font-extrabold text-xl">Users</p>
+            <div className="w-full flex flex-col md:flex-row gap-5">
+                <div className="bg-gray-800 w-full px-8 rounded-md h-24 flex justify-center flex-col items-center border-b-2 border-b-blue-600">
+                    <p className="text-gray-400 font-extrabold text-xl">Orders</p>
 
-                <p className="text-gray-400 font-bold text-xl">1</p>
-            </div>
+                    <p className="text-white font-bold text-xl">1</p>
+                </div>
 
-            <div className="bg-gray-800 columns-3 flex-1 px-8 rounded-md h-24 flex justify-center flex-col items-center">
-                <p className="text-white font-extrabold text-xl">Products</p>
+                <div className="bg-gray-800 w-full px-8 rounded-md h-24 flex justify-center flex-col items-center border-b-2 border-b-blue-600">
+                    <p className="text-gray-400 font-extrabold text-xl">Value in sales</p>
 
-                <p className="text-gray-400 font-bold text-xl">1</p>
+                    <p className="text-white font-bold text-xl">1</p>
+                </div>
+
+                <div className="bg-gray-800 w-full px-8 rounded-md h-24 flex justify-center flex-col items-center border-b-2 border-b-blue-600">
+                    <p className="text-gray-400 font-extrabold text-xl">Itens</p>
+
+                    <p className="text-white font-bold text-xl">1</p>
+                </div>
             </div>
         </div>
     );

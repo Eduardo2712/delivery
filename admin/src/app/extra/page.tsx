84ba0068b146f2b927ef2eaa2879bbb36e1b0b3d
Eduardo Manoel de Lara <52054453+Eduardo2712@users.noreply.github.com>
2024-01-05 21:03:29 -0300

    Update

--- admin/src/app/extra/page.tsx
+++ admin/src/app/extra/page.tsx
@@ -3,6 +3,7 @@
 import CustomBox from "@/components/custom-box";
 import CustomTable from "@/components/custom-table";
 import { getDatatable, remove } from "@/requests/extra.request";
+import { formatBRL } from "@/utils/other";
 import { NextPage } from "next";
 import Link from "next/link";
 import { Column } from "primereact/column";
@@ -26,6 +27,9 @@ const Page: NextPage = () => {
                 <div className="w-full">
                     <CustomTable request={getDatatable} button_edit button_delete url={"/extra"} delete_request={remove} buttons_top={buttons_top}>
                         <Column field="id" header="Id" />
+                        <Column field="ext_name" header="Name" />
+                        <Column field="ext_price" header="Price" body={(e) => formatBRL(e.ext_price)} />
+                        <Column field="ext_status" header="Status" body={(e) => (e.ext_status ? "Enabled" : "Disabled")} />
                     </CustomTable>
                 </div>
             </CustomBox>
--- admin/src/types/request/extra.type.ts
+++ admin/src/types/request/extra.type.ts
@@ -1,5 +1,24 @@
+import { ExtraType } from "../entity/entity.type";
+
 export type ExtraDatatableType = {
     search: string;
     page: number;
     rows_per_page: number;
 };
+
+export type ExtraCreateType = {
+    ext_name: string;
+    ext_price: number;
+    ext_status: boolean;
+    picture: File;
+};
+
+export type ExtraUpdateType = {
+    ext_name: string;
+    ext_price: number;
+    ext_status: boolean;
+    picture: File;
+    new_picture: boolean;
+};
+
+export type ExtraGetType = ExtraType;
